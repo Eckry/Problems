4b913b6ad023ff154a275aca4797483fc0dbcb4e
eckry <112786854+eckry@users.noreply.github.com>
2023-12-08 20:34:29 -0600

    Problem 2627

---
+++ Problem_2627.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+  let time
+  return function(...args) {
+      clearTimeout(time);
+      time = setTimeout(() => {
+          fn(...args)
+      }, t)
+  }
+};
