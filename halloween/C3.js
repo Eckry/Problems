376cf4465dedaa4ef0ea87a6daabf00ddc3038e4
Eckry <112786854+Eckry@users.noreply.github.com>
2024-11-01 18:09:31 -0600

    fix C3 halloween

--- halloween/C3.js
+++ halloween/C3.js
@@ -1,16 +1,25 @@
 function findSafestPath(dream) {
   const n = dream.length;
   const m = dream[0].length;
+  const mem = Array.from({ length: n }, () => Array(m).fill(null));
   function dp(i, j) {
-    if (i >= n - 1 && j >= m - 1) {
+    if (i === n - 1 && j === m - 1) {
       return dream[i][j];
     }
-    if (i === n - 1) return dp(i, j + 1) + dream[i][j];
-    if (j === m - 1) return dp(i + 1, j) + dream[i][j];
-    return Math.min(dp(i + 1, j) + dream[i][j], dp(i, j + 1) + dream[i][j]);
-  }
+    if (mem[i][j] !== null) return mem[i][j];
 
-  return dp(0, 0);
+    if (i === n - 1) mem[i][j] = dp(i, j + 1) + dream[i][j];
+    else if (j === m - 1) mem[i][j] = dp(i + 1, j) + dream[i][j];
+    else
+      mem[i][j] = Math.min(
+        dp(i + 1, j) + dream[i][j],
+        dp(i, j + 1) + dream[i][j]
+      );
+    return mem[i][j];
+  }
+  dp(0, 0);
+
+  return mem[0][0];
 }
 
 const d = [
@@ -19,4 +28,4 @@ const d = [
   [4, 2, 1],
 ];
 
-console.log(findSafestPath(d));
+console.table(findSafestPath(d));
