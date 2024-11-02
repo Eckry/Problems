51634d7b38a2164e875336c94110f18f33367aa4
Eckry <112786854+Eckry@users.noreply.github.com>
2024-11-01 19:15:21 -0600

    fix C3 halloween

--- halloween/C3.js
+++ halloween/C3.js
@@ -1,27 +1,32 @@
 function findSafestPath(dream) {
   const n = dream.length;
   const m = dream[0].length;
-  const mem = Array.from({ length: n }, () => Array(m).fill(null));
+  const memo = Array.from({ length: n }, () => Array(m).fill(null));
+
   function dp(i, j) {
-    if (i === n - 1 && j === m - 1) {
+    if (memo[i][j] !== null) return memo[i][j];
+
+    if (i >= n - 1 && j >= m - 1) {
       return dream[i][j];
     }
-    if (mem[i][j] !== null) return mem[i][j];
+    if (i === n - 1) {
+      return (memo[i][j] = dp(i, j + 1) + dream[i][j]);
+    }
+    if (j === m - 1) {
+      return (memo[i][j] = dp(i + 1, j) + dream[i][j]);
+    }
 
-    if (i === n - 1) mem[i][j] = dp(i, j + 1) + dream[i][j];
-    else if (j === m - 1) mem[i][j] = dp(i + 1, j) + dream[i][j];
-    else
-      mem[i][j] = Math.min(
-        dp(i + 1, j) + dream[i][j],
-        dp(i, j + 1) + dream[i][j]
-      );
-    return mem[i][j];
+    memo[i][j] = Math.min(
+      dp(i + 1, j) + dream[i][j],
+      dp(i, j + 1) + dream[i][j]
+    );
+    return memo[i][j];
   }
-  dp(0, 0);
 
-  return mem[0][0];
+  const res = dp(0, 0);
+
+  return res;
 }
-
 const d = [
   [1, 3, 1],
   [1, 5, 1],
