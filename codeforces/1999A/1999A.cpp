6c1f568b622a46194670ce905114b4bbe04c138a
Eckry <112786854+Eckry@users.noreply.github.com>
2024-12-06 20:18:43 -0600

    1999A

---
+++ codeforces/1999A/1999A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define optimize()              \
+  cin.tie(0);                   \
+  cin.sync_with_stdio(0);       \
+  ios_base::sync_with_stdio(0); \
+  cout.tie(0);
+
+void solve()
+{
+  int n;
+
+  cin >> n;
+
+  cout << (n % 10) + (n / 10) << endl;
+}
+
+int main(void)
+{
+  optimize();
+  int T;
+  cin >> T;
+  // T = 1;
+  while (T--)
+    solve();
+}
