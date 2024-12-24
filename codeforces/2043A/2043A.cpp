8f7f8880eb16a4225318a52441c55c2c7a19adcd
Eckry <112786854+Eckry@users.noreply.github.com>
2024-12-24 16:11:42 -0600

    2043A

---
+++ codeforces/2043A/2043A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define optimize()              \
+  cin.tie(0);                   \
+  cin.sync_with_stdio(0);       \
+  ios_base::sync_with_stdio(0); \
+  cout.tie(0);
+
+void solve()
+{
+  long long int n;
+  cin >> n;
+  long long int count = 1;
+  while (n >= 1)
+  {
+    n /= 4;
+    count *= 2;
+  }
+  cout << count / 2 << "\n";
+}
+
+int main(void)
+{
+  optimize();
+  int T;
+  cin >> T;
+  // T = 1;
+  while (T--)
+    solve();
+}
