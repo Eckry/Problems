32ff8871ef1a030c2b685e8e98292f2379044169
Eckry <112786854+Eckry@users.noreply.github.com>
2025-01-20 15:16:15 -0600

    fix 2060D

--- codeforces/2060D/2060D.cpp
+++ codeforces/2060D/2060D.cpp
@@ -18,46 +18,13 @@ void solve()
 
   for (int i = 0; i < n - 1; i++)
   {
-    if (a[i] < a[i + 1])
-    {
-      a[i + 1] -= a[i];
-      a[i] = 0;
-
-      continue;
-    }
     if (a[i] > a[i + 1])
     {
-      if (i != 0)
-      {
-        if (a[i - 1] < a[i])
-          a[i] -= a[i - 1];
-        else
-        {
-          cout << "NO" << "\n";
-          return;
-        }
-        if (a[i] > a[i + 1])
-        {
-          cout << "NO" << "\n";
-          return;
-        }
-      }
-      else
-      {
-        cout << "NO" << "\n";
-        return;
-      }
-      continue;
+      cout << "NO" << "\n";
+      return;
     }
-
-    if (a[i] == a[i + 1] && i != 0)
-    {
-      if (a[i - 1] == 0)
-      {
-        a[i] = 0;
-        a[i + 1] = 0;
-      }
-    }
+    a[i + 1] -= a[i];
+    a[i] = 0;
   }
   cout << "YES" << "\n";
 }
