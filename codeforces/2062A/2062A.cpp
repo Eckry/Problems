dd87e78976a7a6210a1884463d21b1d513ccac0e
Eckry <112786854+Eckry@users.noreply.github.com>
2025-01-26 19:36:13 -0600

    2062A

---
+++ codeforces/2062A/2062A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define optimize()              \
+  cin.tie(0);                   \
+  cin.sync_with_stdio(0);       \
+  ios_base::sync_with_stdio(0); \
+  cout.tie(0);
+
+void solve()
+{
+  string s;
+  cin >> s;
+  int cnt = 0;
+  for (auto it : s)
+  {
+    if (it == '1')
+      cnt++;
+  }
+  cout << cnt << "\n";
+}
+
+int main(void)
+{
+  optimize();
+  int T;
+  cin >> T;
+  // T = 1;
+  while (T--)
+    solve();
+}
