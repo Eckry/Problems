a84e5a70a8d545c141a8e48db398cea7f6e0a86f
Eckry <112786854+Eckry@users.noreply.github.com>
2025-02-20 21:13:26 -0600

    1988A

---
+++ codeforces/1988A/1988A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define optimize()              \
+  cin.tie(0);                   \
+  cin.sync_with_stdio(0);       \
+  ios_base::sync_with_stdio(0); \
+  cout.tie(0);
+
+void solve()
+{
+  int n, k;
+  cin >> n >> k;
+  int op = 0;
+  while (n > 1)
+  {
+    n -= k - 1;
+    op++;
+  }
+  cout << op << "\n";
+}
+
+int main(void)
+{
+  optimize();
+  int T;
+  cin >> T;
+  // T = 1;
+  while (T--)
+    solve();
+}
