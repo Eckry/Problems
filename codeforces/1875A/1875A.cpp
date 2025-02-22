1cefa14a230e4a582e8e9e5480d2e33ba2c7d38c
Eckry <112786854+Eckry@users.noreply.github.com>
2025-02-21 20:22:45 -0600

    1875A

---
+++ codeforces/1875A/1875A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define optimize()              \
+  cin.tie(0);                   \
+  cin.sync_with_stdio(0);       \
+  ios_base::sync_with_stdio(0); \
+  cout.tie(0);
+
+void solve()
+{
+  ll a, b, n;
+  cin >> a >> b >> n;
+  vector<ll> x(n);
+  for (auto &c : x)
+    cin >> c;
+  ll ans = b - 1;
+  for(auto c : x) {
+    ans += min(c, a - 1);
+  }
+  cout << ans + 1 << "\n";
+}
+
+int main(void)
+{
+  optimize();
+  int T;
+  cin >> T;
+  // T = 1;
+  while (T--)
+    solve();
+}
