ac5e587af232f23559590173bf2ca7e991f6024c
Eckry <112786854+Eckry@users.noreply.github.com>
2025-06-12 11:45:08 -0600

    2118A

---
+++ codeforces/2118A/2118A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define optimize()              \
+  cin.tie(0);                   \
+  cin.sync_with_stdio(0);       \
+  ios_base::sync_with_stdio(0); \
+  cout.tie(0);
+
+void solve()
+{
+  int n, k;
+  cin >> n >> k;
+
+  for (int i = 0; i < k; i++)
+  {
+    cout << 1 << "";
+  }
+  for (int i = k; i < n; i++)
+  {
+    cout << 0 << "";
+  }
+  cout << "\n";
+}
+
+int main(void)
+{
+  optimize();
+  int T;
+  cin >> T;
+  // T = 1;
+  while (T--)
+    solve();
+}
