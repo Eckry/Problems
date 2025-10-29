ba8f09384dd79e69d81e86a064d5f0255f98918d
Eckry <112786854+Eckry@users.noreply.github.com>
2025-10-28 22:08:44 -0600

    105873J

--- codeforces/105873J/105873J.cpp
+++ codeforces/105873J/105873J.cpp
@@ -4,42 +4,38 @@ using namespace std;
 
 #define ll long long
 
-#define optimize()                                                             \
-  cin.tie(0);                                                                  \
-  cin.sync_with_stdio(0);                                                      \
-  ios_base::sync_with_stdio(0);                                                \
-  cout.tie(0);
-
 void solve() {
   int n;
   cin >> n;
   string s;
   cin >> s;
-  vector<int> bits(n, 0);
-  bits[0] = 1 << (s[0] - 'a');
-  unordered_map<int, int> mp;
-  mp[bits[0]] = 1;
-  mp[0] = 0;
+
+  int pref = 0;
   int ans = 0;
-  for (int i = 1; i < n; i++) {
-    bits[i] = (bits[i - 1] ^ (1 << (s[i] - 'a')));
-    if (mp.count(bits[i])) {
-      ans = max(i - mp[bits[i]] + 1, ans);
-    } else {
-      mp[bits[i]] = i + 1;
-    }
+  vector<int> mp((1 << 26), -1);
+  for (int i = 0; i < n; i++) {
+    pref ^= 1 << (s[i] - 'a');
     for (int j = 0; j < 26; j++) {
-      int x = (1 << j);
-      if (mp.count(bits[i] ^ x)) {
-        ans = max(ans, i + 1 - mp[bits[i] ^ x]);
+      int lol = pref ^ (1 << j);
+      if (mp[lol] != -1) {
+        ans = max(ans, i - mp[lol]);
       }
+    }
+    if (mp[pref] == -1) {
+      mp[pref] = i;
+    } else {
+      ans = max(ans, i - mp[pref]);
+    }
+    if (pref == 0 || __builtin_popcount(pref) == 1) {
+      ans = max(ans, i + 1);
     }
   }
   cout << ans << "\n";
 }
 
 int main() {
-  optimize();
+  ios_base::sync_with_stdio(0);
+  cin.tie(nullptr);
   int T;
   T = 1;
   while (T--)
