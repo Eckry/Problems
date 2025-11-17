3e6d748022714b8fac43f54038bf624955aaef7c
Eckry <112786854+Eckry@users.noreply.github.com>
2025-11-17 17:33:39 -0600

    2166A

---
+++ codeforces/2166A/2166A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+void solve() {
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+
+  vector<int> cnt(26, 0);
+  for (int i = 0; i < n; i++) {
+    cnt[s[i] - 'a']++;
+  }
+  int ans = n - cnt[s[n - 1] - 'a'];
+  cout << ans << "\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(nullptr);
+  int T;
+  cin >> T;
+  while (T--)
+    solve();
+}
