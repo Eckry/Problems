2b248bf99ed0984a7fca0b4a2dac47921e958f03
Eckry <112786854+Eckry@users.noreply.github.com>
2025-11-25 12:18:51 -0600

    2163A

---
+++ codeforces/2163A/2163A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (auto &c : a)
+    cin >> c;
+  sort(a.begin(), a.end());
+  for (int i = 1; i < n - 1; i += 2) {
+    if (a[i] != a[i + 1]) {
+      cout << "NO\n";
+      return;
+    }
+  }
+  cout << "YES\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(nullptr);
+  int T;
+  cin >> T;
+  while (T--)
+    solve();
+}
