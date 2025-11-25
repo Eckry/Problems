82b57ce2abeb0d10ba43ba947640a3ebb90ad407
Eckry <112786854+Eckry@users.noreply.github.com>
2025-11-25 12:15:30 -0600

    codechef WOLFDOWN

---
+++ codechef/WOLFDOWN.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+void solve() {
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  int ans = 0;
+  while (ans < n && s[ans] == '0')
+    ans++;
+  cout << ans << "\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(nullptr);
+  int T;
+  cin >> T;
+  while (T--)
+    solve();
+}
