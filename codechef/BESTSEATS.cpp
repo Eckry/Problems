b1681c14e5c7e7daf961a401d4d07ffc15fe48a8
Eckry <112786854+Eckry@users.noreply.github.com>
2025-12-13 18:22:21 -0600

    BESTSEATS

---
+++ codechef/BESTSEATS.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (auto &c : a)
+    cin >> c;
+  int ans = 1000000;
+  for (int i = 0; i < n - 1; i++) {
+    ans = min(ans, a[i] + a[i + 1]);
+  }
+  cout << ans << "\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(nullptr);
+  int T;
+  cin >> T;
+  while (T--)
+    solve();
+}
