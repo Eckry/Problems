065e2619aa97d6994b3a8aae78736dcfe73713b0
Eckry <112786854+Eckry@users.noreply.github.com>
2025-12-26 17:03:09 -0600

    codechef STOPCOUNT

---
+++ codechef/STOPCOUNT.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+void solve() {
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  int ones = 0;
+  int zeros = 0;
+  int ans = 0;
+  for (int i = 0; i < n; i++) {
+    if (s[i] == '1')
+      ones++;
+    else
+      zeros++;
+    if (ones > zeros)
+      ans++;
+  }
+  cout << ans << "\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(nullptr);
+  int T;
+  cin >> T;
+  while (T--)
+    solve();
+}
