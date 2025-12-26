c39ee93e61a2325a29d4c88338fab2a484117213
Eckry <112786854+Eckry@users.noreply.github.com>
2025-12-26 16:56:02 -0600

    2179A

---
+++ codeforces/2179A/2179A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+void solve() {
+  int k, x;
+  cin >> k >> x;
+  cout << k * x + 1 << "\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(nullptr);
+  int T;
+  cin >> T;
+  while (T--)
+    solve();
+}
